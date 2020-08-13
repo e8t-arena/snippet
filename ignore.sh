7787db229024901bed26af858772318b8c08311d
Peter Lau <superpeterlau@outlook.com>
2020-08-13 13:22:56 +0800

    add executable file to gitignore

---
+++ ignore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# find . -type f -perm +111  -not -path '**/node_modules/*' -not -path '**/.git/*' >> .gitignore
+
+paths=(
+  **/node_modules/*
+  **/.git/*
+)
+
+not_paths=''
+
+for path in ${paths[@]}
+do
+  # echo $path
+  not_paths=$not_paths' -not -path '$path
+done
+
+echo $not_paths
+
+find . -type f -perm +111 $not_paths >> .gitignore
+# awk '!NF || !seen[$0]++' .gitignore
+result=$(awk '!NF || !seen[$0]++' .gitignore)
+echo "$result" > .gitignore
